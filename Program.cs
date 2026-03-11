96ed1b17746d669f9604036f899c5e89a51037b6
gabrieldetorres <gabrieldetorres21@gmail.com>
2026-03-11 20:10:06 +0800

    added ClassLibrary

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 using System.Threading;
+using ClassLibrary1;
+using Overtime;
+
 
 namespace PROJECT_REPO
 {
     internal class Program
     {
-
         static TimeSpan time_in = new TimeSpan(9, 0, 0);
         static TimeSpan time_out = new TimeSpan(17, 0, 0);
+
         static void Main(string[] args)
         {
+
+            timIn_timeOut emp = new timIn_timeOut();
+            Overtime1 ot = new Overtime1();
+
             Console.WriteLine("Time In / Time Out ");
 
             Console.Write("Enter Employee ID: ");
@@ -26,45 +33,15 @@ namespace PROJECT_REPO
             Console.Write("Enter Time Out: ");
             TimeSpan timeOut = TimeSpan.Parse(Console.ReadLine());
 
-
             
-
-            Console.WriteLine("\n RECORD ");
             Console.WriteLine("Employee ID: " + id);
             Console.WriteLine("Employee Name: " + name);
             Console.WriteLine("Time In: " + timeIn);
             Console.WriteLine("Time Out: " + timeOut);
 
-            if (timeIn > time_in)
-            {
-                Console.WriteLine("Status: Late");
-            }
-            else if (timeIn < time_in)
-            {
-                Console.WriteLine("Early Bird");
-            }
-            else
-            {
-                Console.WriteLine("Status: On Time");
-            }
+            ot.Overtimehours(emp);
+            ot.TotalHours(emp);
 
-
-            if (timeOut > time_out)
-            {
-                Console.WriteLine("Overtime");
-            }
-            else
-            {
-                Console.WriteLine("No Overtime");
-            }
-
-            Hours(timeIn, timeOut);
-        }
-
-        static void Hours(TimeSpan timein, TimeSpan timeout)
-        {
-            TimeSpan totalHours = timeout - timein;
-            Console.WriteLine("Total Working Hours: " + totalHours);
         }
     }
 }
