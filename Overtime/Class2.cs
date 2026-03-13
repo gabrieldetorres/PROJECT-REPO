fa21215a8442e28f8b3b78b5572aee6190da3419
gabrieldetorres <gabrieldetorres21@gmail.com>
2026-03-13 12:20:58 +0800

    Added two ClassLibrary Overtime and TimeIn_TimeOut

---
+++ Overtime/Class2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeIn_TimeOut;
+
+namespace Overtime
+{
+    public class Class2
+    {
+        TimeSpan time_in = new TimeSpan(9, 0, 0);
+        TimeSpan time_out = new TimeSpan(17, 0, 0);
+
+        public void Overtimehours(Class1 emp)
+        {
+            if (emp.TimeIn > time_in)
+            {
+                Console.WriteLine("Status: Late");
+            }
+            else if (emp.TimeIn < time_in)
+            {
+                Console.WriteLine("Early Bird");
+            }
+            else
+            {
+                Console.WriteLine("Status: On Time ");
+            }
+
+            if (emp.Timeout > time_out)
+            {
+                Console.WriteLine("Overtime");
+            }
+            else
+            {
+                Console.WriteLine("No Overtime");
+            }
+        }
+
+        public void TotalHours(Class1 emp)
+        {
+            TimeSpan hours = emp.Timeout - emp.TimeIn;
+            Console.WriteLine("Total Working Hours: " + hours);
+        }
+    }
+}
---
+++ TimeManagementSystem/Program.cs
@@ -0,0 +1,55 @@
+using System.Threading;
+using TimeIn_TimeOut;
+using Overtime;
+
+namespace PROJECT_REPO
+{
+    internal class Program
+    {
+        static TimeSpan time_in = new TimeSpan(9, 0, 0);
+        static TimeSpan time_out = new TimeSpan(17, 0, 0);
+
+        static void Main(string[] args)
+        {
+
+
+
+            Class1 c1 = new Class1();
+            Class2 c2 = new Class2();
+
+
+
+            Console.WriteLine("Time In - Time Out ");
+
+            Console.Write("Enter Employee Id: ");
+            c1.Id = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Enter Employee Name: ");
+            c1.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter 24 hour clock:");
+            Console.WriteLine();
+
+            Console.Write("Enter Time In: ");
+            c1.TimeIn = TimeSpan.Parse(Console.ReadLine());
+
+            Console.Write("Enter Time Out: ");
+            c1.Timeout = TimeSpan.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("\n ============= RECORD ============= ");
+            Console.WriteLine("Employee Id: " + c1.Id);
+            Console.WriteLine("Employee Name " + c1.Name);
+            Console.WriteLine("Time In " + c1.TimeIn);
+            Console.WriteLine("Time Out " + c1.Timeout);
+
+            c2.Overtimehours(c1);
+            c2.TotalHours(c1);
+
+        }
+
+    }
+}
+
